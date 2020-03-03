131eaf718f9434145a8f4e977a026fd722d6ca8e
Michał Marchewa <michalmarchewa@gmail.com>
2020-03-03 23:39:04 +0100

    While loops

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
-var myAge = 25;
+var age = 5;
 
-if (myAge < 18 || myAge > 30 || myAge ===25){
-    document.write("You can come, you cool dude")
-} else {
-    document.write("you ain't coming")
+while(age <= 10){
+    document.writeln("I'm " + age +"!")
+    
+    age++;
 }
+
+document.write("you are over 10")
