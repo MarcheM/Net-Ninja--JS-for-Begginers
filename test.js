1fcc40ad955dc4d9bd93f3ce94be191a84463eb0
Michał Marchewa <michalmarchewa@gmail.com>
2020-03-04 23:24:08 +0100

    variable scope

--- test.js
+++ test.js
@@ -1,7 +1,14 @@
 function getAverage (a,b){
     
-    var average = (a + b)/2;
+    var average = (a + b)/2; //local variable
     return average;
 }
-var myResult = getAverage(8,5);
-console.log("the average is " + myResult);
+var myResult = getAverage(8,5); //global variable
+//console.log("the average is " + myResult);
+
+function logResult(){
+
+    console.log("the average is " + myResult + " inside the function");
+}
+
+logResult();
