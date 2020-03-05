01614ae979dac7c5719a4171cc1a770dc0cd96ce
Michał Marchewa <michalmarchewa@gmail.com>
2020-03-05 22:23:05 +0100

    Strings

--- test.js
+++ test.js
@@ -1,13 +1,19 @@
-var a = 5;
-var b = "apple";
+ var myString = 'I\'m a "fun" ninja String';
 
-if (isNaN(b) || isNaN(a)){
+ console.log(myString.length);
+ console.log(myString.toUpperCase());
+ console.log(myString.toLowerCase());
+ console.log(myString.indexOf("fun"));
+ console.log(myString.indexOf("ninja"));
+    if (myString.indexOf("ninja") === -1){
+        console.log("the word ninja is not in the string");
+    }else{
+        console.log("The word ninja starts at position " + myString.indexOf("ninja"))
+    }
+ var string1 = "abc";
+ var string2 = "bcd";
+ console.log(string1 === string2);
 
-console.log("that ain't a number");
+ console.log(string1 < string2);
 
-}else{
-    console.log(a*b);
-}
-
-
-
+ 
